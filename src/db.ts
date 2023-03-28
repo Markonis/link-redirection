7418189dd7812569c37cddb021842678a4b9c8e0
Marko Pavlovic <contact.pavlovic@gmail.com>
2023-03-28 18:26:14 +0200

    Use db connection string

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,7 @@
 import { Pool, QueryResult } from 'pg';
 
 const pool = new Pool({
-	user: process.env.DB_USER,
-	host: process.env.DB_HOST,
-	database: process.env.DB_NAME,
-	password: process.env.DB_PASSWORD,
-	port: parseInt(process.env.DB_PORT || '5432'),
+	connectionString: process.env.DATABASE_URL,
 });
 
 // Add the following function below the pool initialization
