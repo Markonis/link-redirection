e4d6eacd0a2f353bc58bec87939588c6b8722060
imrany <imranmat254@gmail.com>
2023-03-31 22:46:30 +0300

    change port from 3000 to 8000

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { logLinkClick } from './db';
 import cors from "cors";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 // Add custom properties to the Request object
 declare global {
