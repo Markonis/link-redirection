8ff627b89ed4843fae078e00af82d94e248ffb15
Marko Pavlovic <contact.pavlovic@gmail.com>
2023-05-11 00:15:14 +0200

    Update link defs

--- src/index.ts
+++ src/index.ts
@@ -8,44 +8,44 @@ import { useTreblle } from 'treblle';
 const app = express();
 
 useTreblle(app, {
-	apiKey: process.env.TREBLLE_API_KEY,
-	projectId: process.env.TREBLLE_PROJECT_ID,
+    apiKey: process.env.TREBLLE_API_KEY,
+    projectId: process.env.TREBLLE_PROJECT_ID,
 })
 
 const port = process.env.PORT || 3000;
 
 // Add custom properties to the Request object
 declare global {
-	namespace Express {
-		interface Request {
-			country: string;
-			videoId?: string;
-		}
-	}
+    namespace Express {
+        interface Request {
+            country: string;
+            videoId?: string;
+        }
+    }
 }
 
 app.use(extractRequestInfo);
 
 app.get('/:slug', async (req: Request, res) => {
-	const { slug } = req.params;
-	const { country, videoId } = req;
+    const { slug } = req.params;
+    const { country, videoId } = req;
 
-	// Find the link definition
-	const linkDefinition: LinkDefinition =
-		linkDefinitions.find((def) => def.slug === slug) || fallbackDefinition;
+    // Find the link definition
+    const linkDefinition: LinkDefinition =
+        linkDefinitions.find((def) => def.slug === slug) || fallbackDefinition;
 
-	// Map the user's country to the appropriate Amazon store country
-	const amazonCountry = countryMapping[country] || 'US';
+    // Map the user's country to the appropriate Amazon store country
+    const amazonCountry = countryMapping[country] || 'US';
 
-	// Find the appropriate URL for the mapped Amazon store country or the default URL
-	const redirectUrl = linkDefinition.links[amazonCountry] || linkDefinition.links['US'];
+    // Find the appropriate URL for the mapped Amazon store country or the default URL
+    const redirectUrl = linkDefinition.links[amazonCountry] || linkDefinition.links['DEFAULT'];
 
-	// Log the link click to the Postgres database
-	await logLinkClick(slug, country, linkDefinition.tags, redirectUrl, videoId);
+    // Log the link click to the Postgres database
+    await logLinkClick(slug, country, linkDefinition.tags, redirectUrl, videoId);
 
-	res.redirect(redirectUrl);
+    res.redirect(redirectUrl);
 });
 
 app.listen(port, () => {
-	console.log(`Server started on port ${port}`);
+    console.log(`Server started on port ${port}`);
 });
--- src/link-definitions.ts
+++ src/link-definitions.ts
@@ -1,138 +1,150 @@
 export interface LinkDefinition {
-    slug: string;
-    tags: string[];
-    links: {
-        US: string;
-        [country: string]: string;
-    };
+	slug: string;
+	tags: string[];
+	links: {
+		DEFAULT: string;
+		[country: string]: string;
+	};
 }
 
 export const linkDefinitions: LinkDefinition[] = [
-    {
-        slug: 'backpack',
-        tags: ['backpack'],
-        links: {
-            US: 'https://amzn.to/3BDjjqG'
-        },
-    },
-    {
-        slug: 'vissles-keyboard',
-        tags: ['keyboard'],
-        links: {
-            US: 'https://amzn.to/3cNiYJB'
-        }
-    },
-    {
-        slug: 'michals-keyboard',
-        tags: ['keyboard'],
-        links: {
-            US: 'https://amzn.to/3xGschW'
-        }
-    },
-    {
-        slug: 'mx-master-3s',
-        tags: ['mouse'],
-        links: {
-            US: 'https://amzn.to/3zZZBF9'
-        }
-    },
-    {
-        slug: 'michals-mouse',
-        tags: ['mouse'],
-        links: {
-            US: 'https://amzn.to/3BXGHAI'
-        }
-    },
-    {
-        slug: 'cleaning-tool',
-        tags: ['cleaning'],
-        links: {
-            US: 'https://amzn.to/3CZZVpV'
-        }
-    },
-    {
-        slug: 'earbuds',
-        tags: ['earbuds'],
-        links: {
-            US: 'https://amzn.to/3RYEVUx'
-        }
-    },
-    {
-        slug: 'lofree',
-        tags: ['keyboard'],
-        links: {
-            US: 'https://amzn.to/3W0OBkI'
-        }
-    },
-    {
-        slug: 'floating-plant',
-        tags: ['plant', 'desk-accessory'],
-        links: {
-            US: 'https://amzn.to/3gXzU1S'
-        }
-    },
-    {
-        slug: 'productivity-mouse',
-        tags: ['mouse'],
-        links: {
-            US: 'https://amzn.to/3OJEEoD'
-        }
-    },
-    {
-        slug: 'headphones',
-        tags: ['headphones'],
-        links: {
-            US: 'https://amzn.to/3IGTP0X'
-        }
-    },
-    {
-        slug: 'productivity-keyboard',
-        tags: ['keyboard'],
-        links: {
-            US: 'https://amzn.to/3Hw7k2B'
-        }
-    },
-    {
-        slug: 'led-matrix-clock',
-        tags: ['clock', 'bluetooth-speaker', 'desk-accessory'],
-        links: {
-            US: 'https://amzn.to/3zIKwHO'
-        }
-    },
-    {
-        slug: 'github',
-        tags: ['github'],
-        links: {
-            US: 'https://github.com/Markonis'
-        }
-    },
-    {
-        slug: 'one-menu',
-        tags: ['one-menu'],
-        links: {
-            US: 'https://withmarko.com/one-menu'
-        }
-    },
-    {
-        slug: 'typing-game',
-        tags: ['game', 'project'],
-        links: {
-            US: 'https://type.withmarko.com'
-        }
-    },
-    {
-        slug: 'boox',
-        tags: ['tablet'],
-        links: {
-            US: 'https://shop.boox.com/?ref=gisG1RQxpV9suI'
-        }
-    }
+	{
+		slug: 'backpack',
+		tags: ['backpack'],
+		links: {
+			DEFAULT: 'https://amzn.to/3BDjjqG'
+		},
+	},
+	{
+		slug: 'vissles-keyboard',
+		tags: ['keyboard'],
+		links: {
+			DEFAULT: 'https://amzn.to/3cNiYJB'
+		}
+	},
+	{
+		slug: 'michals-keyboard',
+		tags: ['keyboard'],
+		links: {
+			DEFAULT: 'https://amzn.to/3xGschW',
+			DE: 'https://amzn.to/432Z0PN'
+		}
+	},
+	{
+		slug: 'mx-master-3s',
+		tags: ['mouse'],
+		links: {
+			DEFAULT: 'https://amzn.to/3zZZBF9',
+			DE: 'https://amzn.to/3NW0vuH'
+		}
+	},
+	{
+		slug: 'michals-mouse',
+		tags: ['mouse'],
+		links: {
+			DEFAULT: 'https://amzn.to/3BXGHAI',
+			DE: 'https://amzn.to/42nnlQ8'
+
+		}
+	},
+	{
+		slug: 'cleaning-tool',
+		tags: ['cleaning'],
+		links: {
+			DEFAULT: 'https://amzn.to/3CZZVpV',
+			DE: 'https://amzn.to/3HWvdjq'
+		}
+	},
+	{
+		slug: 'earbuds',
+		tags: ['earbuds'],
+		links: {
+			DEFAULT: 'https://amzn.to/3RYEVUx',
+			DE: 'https://amzn.to/44SR59s'
+		}
+	},
+	{
+		slug: 'lofree',
+		tags: ['keyboard'],
+		links: {
+			DEFAULT: 'https://amzn.to/3W0OBkI',
+
+		}
+	},
+	{
+		slug: 'floating-plant',
+		tags: ['plant', 'desk-accessory'],
+		links: {
+			DEFAULT: 'https://amzn.to/3gXzU1S',
+			DE: 'https://amzn.to/3LM1wTm'
+		}
+	},
+	{
+		slug: 'productivity-mouse',
+		tags: ['mouse'],
+		links: {
+			DEFAULT: 'https://amzn.to/3OJEEoD',
+			DE: 'https://amzn.to/3LTRmAf'
+		}
+	},
+	{
+		slug: 'headphones',
+		tags: ['headphones'],
+		links: {
+			DEFAULT: 'https://amzn.to/3IGTP0X',
+			DE: 'https://amzn.to/42qHTam'
+		}
+	},
+	{
+		slug: 'productivity-keyboard',
+		tags: ['keyboard'],
+		links: {
+			DEFAULT: 'https://amzn.to/3Hw7k2B',
+			DE: 'https://amzn.to/42F1ByQ'
+		}
+	},
+	{
+		slug: 'led-matrix-clock',
+		tags: ['clock', 'bluetooth-speaker', 'desk-accessory'],
+		links: {
+			DEFAULT: 'https://amzn.to/3zIKwHO',
+			DE: 'https://amzn.to/3W0xqAb'
+		}
+	},
+	{
+		slug: 'github',
+		tags: ['github'],
+		links: {
+			DEFAULT: 'https://github.com/Markonis'
+		}
+	},
+	{
+		slug: 'one-menu',
+		tags: ['one-menu'],
+		links: {
+			DEFAULT: 'https://withmarko.com/one-menu'
+		}
+	},
+	{
+		slug: 'typing-game',
+		tags: ['game', 'project'],
+		links: {
+			DEFAULT: 'https://type.withmarko.com'
+		}
+	},
+	{
+		slug: 'boox',
+		tags: ['tablet'],
+		links: {
+			DEFAULT: 'https://shop.boox.com/?ref=gisG1RQxpV9suI'
+		}
+	}
 ];
 
 export const fallbackDefinition: LinkDefinition = {
-    slug: 'fallback',
-    tags: ['fallback'],
-    links: {
-        US: 'https://www.amazon.com/fallback-product',
-    },
+	slug: 'fallback',
+	tags: ['fallback'],
+	links: {
+		DEFAULT: 'https://www.amazon.com',
+	},
 };
