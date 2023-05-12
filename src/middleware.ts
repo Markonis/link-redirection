29484198c5279db5f8133fc57d2a5997b30f0d61
Marko Pavlovic <contact.pavlovic@gmail.com>
2023-05-12 19:44:49 +0200

    Handle multple proxies in x-forwarded-for

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import geoip from 'geoip-lite';
 export const extractRequestInfo = (req: Request, res: Response, next: NextFunction) => {
 	// Extract IP address
 	const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || '';
-	const ipString = typeof ip === 'string' ? ip : ip[0];
+	const ipString = typeof ip === 'string' ? ip.split(', ')[0] : ip[0];
 
 	// Extract country
 	const geo = geoip.lookup(ipString);
